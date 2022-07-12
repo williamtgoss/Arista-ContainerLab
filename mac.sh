53a68ea6a7b8d2bd7b4003ccd2b632af9a75cf62
Dimitri Capetz <dimitri.capetz@gmail.com>
2022-07-12 17:10:01 -0500

    Adjusted Topo and Base Configs

--- mac.sh
+++ mac.sh
@@ -16,6 +16,8 @@ sudo sed -i 's/..:..:..:..:..:../00:1c:73:24:00:00/' clab-quadGraphics/WISUS01-D
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:25:00:00/' clab-quadGraphics/WISUS01-DC1-LEAF4/flash/system_mac_address
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:26:00:00/' clab-quadGraphics/CORE1/flash/system_mac_address
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:27:00:00/' clab-quadGraphics/CORE2/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:28:00:00/' clab-quadGraphics/WISUS01-DC1-FW/flash/system_mac_address
+sudo sed -i 's/..:..:..:..:..:../00:1c:73:29:00:00/' clab-quadGraphics/WIWEA01-DC1-FW/flash/system_mac_address
 sudo cp configs/cv-onboarding-token clab-quadGraphics/WIWEA01-DC1-SPINE1/flash/cv-onboarding-token
 sudo cp configs/cv-onboarding-token clab-quadGraphics/WIWEA01-DC1-SPINE2/flash/cv-onboarding-token
 sudo cp configs/cv-onboarding-token clab-quadGraphics/WIWEA01-DC1-LEAF1/flash/cv-onboarding-token
